e1ee88ce6918aaa2c26e23c36fa2368700753bb4
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-07-08 12:27:06 +0200

    Update VolunteerDetails.jsx
    Getting data from backend

--- src/pages/VolunteerDetails/VolunteerDetails.jsx
+++ src/pages/VolunteerDetails/VolunteerDetails.jsx
@@ -1,4 +1,6 @@
 // import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
 
 
@@ -6,9 +8,32 @@ const VolunteerDetails = () => {
 
 const params= useParams();
 
+console.log(params.id)
+
+const [data, setData]=useState([])
+  const apiUrl = 'https://reviver-backend.herokuapp.com'
+  const token = localStorage.getItem('token')
+
+  const authAxios = axios.create({
+    baseURL:apiUrl,
+    headers: {Authorization :`Bearer ${token}`}
+  })
+      
+
+  useEffect(()=> {
+    authAxios.get(`${apiUrl}/volunteers/`+ params.id)
+    .then(res=> {
+      console.log("Data is", res.data)
+      setData(res.data)
+    })
+    .catch(err=>console.log(err))
+  }, 
+ // eslint-disable-next-line
+  [])
+
   return (
     <>
-    <div>id</div>
+    <div>id : {data._id}</div>
     <div>name</div>
     <div>email</div>
     <div>Client stuff : {JSON.stringify(params)}</div>
