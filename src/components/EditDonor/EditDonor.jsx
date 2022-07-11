637f970a9af472efa45222adbbf3f5e9b4f9ad09
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-07-11 11:14:08 +0200

    update

---
+++ src/components/EditDonor/EditDonor.jsx
@@ -0,0 +1,169 @@
+import {Button, Grid, Paper, TextField, Typography } from '@mui/material'
+import axios from 'axios';
+import React, { useState } from 'react';
+import "./EditDonor.scss";
+
+
+const EditDonor = ({props, data}) => {
+    const [values, setValues] = useState({
+        name:data.name,
+        surname:data.surname,
+        email:data.email,
+        phone: data.phone,
+        amount: data.numberOfShoes,
+        droppoint: data.dropPoints,
+
+        
+      });
+    
+      console.log(values)
+
+        const apiUrl = 'https://reviver-backend.herokuapp.com'
+        const token = localStorage.getItem('token')
+
+        const authAxios = axios.create({
+            baseURL:apiUrl,
+            headers: {Authorization :`Bearer ${token}`}
+        })
+            
+
+
+    
+      const handleSubmit = (e) =>{
+    
+        e.preventDefault();
+        authAxios.put(`${apiUrl}/donors/`+ props,{
+            name:values.name,
+            surname:values.surname,
+            email:values.email,
+            phone: values.phone,
+            numberOfShoes: values.amount,
+            dropPoints: values.droppoint,
+        })
+        .then((res)=>{
+            console.log(res.message);
+            alert("Data successfully UPDATED")
+           
+        })
+        .catch(err=>console.error(err))
+      }
+  return (
+
+    <Paper elevation={2} sx={{ padding: 2 }}>
+		
+		<Typography variant="h5">
+			Edit Data
+		</Typography>
+		<form onSubmit={handleSubmit}>
+			<Grid container direction="column" spacing={2}
+					style={{ fontSize:"10px" }}>
+				<Grid item>
+						<TextField
+							type="string"
+							fullWidth
+							label="Enter your first name"
+							placeholder={data.name}
+							variant="standard"
+							size="small"
+							color="success"
+							onChange={(e)=>setValues({...values, name:e.target.value})}
+						/>
+				</Grid>
+				<Grid item>
+						<TextField
+							type="string"
+							fullWidth
+							label="Enter your last name"
+							placeholder={data.surname}
+							variant="standard"
+							size="small"
+							color="success"
+							onChange={(e)=>setValues({...values, surname:e.target.value})}
+						/>
+				</Grid>
+				<Grid item>
+						<TextField
+							type="email"
+							fullWidth
+							label="Enter your email"
+							placeholder={data.email}
+							variant="standard"
+							size="small"
+							color="success"
+							onChange={(e)=>setValues({...values, email:e.target.value})}
+						/>
+				</Grid>
+
+                <Grid item>
+						<TextField
+							type="string"
+							fullWidth
+							label="Enter your phone"
+							placeholder={data.phone}
+							variant="standard"
+							size="small"
+							color="success"
+							onChange={(e)=>setValues({...values, phone:e.target.value})}
+						/>
+				</Grid>
+
+                <Grid item>
+						<TextField
+							type="string"
+							fullWidth
+							label="Enter your choice of job"
+							placeholder={data.choiceOfJob}
+							variant="standard"
+							size="small"
+							color="success"
+							onChange={(e)=>setValues({...values, job:e.target.value})}
+						/>
+				</Grid>
+
+                <Grid item>
+						<TextField
+							type="string"
+							fullWidth
+							label="Enter your choice of location"
+							placeholder={data.location}
+							variant="standard"
+							size="small"
+							color="success"
+							onChange={(e)=>setValues({...values, location:e.target.value})}
+						/>
+				</Grid>
+
+                <Grid item>
+						<TextField
+							type="string"
+							fullWidth
+							label="Enter your choice of duration"
+							placeholder={data.duration}
+							variant="standard"
+							size="small"
+							color="success"
+							onChange={(e)=>setValues({...values, duration:e.target.value})}
+						/>
+				</Grid>
+
+                <Grid item>
+					<Button type="submit" fullWidth variant="outlined" color="success">
+						UPDATE
+					</Button>
+                </Grid>
+
+					
+
+					
+			</Grid>
+		</form>
+	
+        
+
+	</Paper>
+    
+        
+  )
+}
+
+export default EditDonor
---
+++ src/pages/DonorDetails/DonorDetails.jsx
@@ -0,0 +1,120 @@
+// import React, { useEffect, useState } from 'react'
+import "./DonorDetails.scss"
+import { Grid, Paper,Typography } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom'
+import Navbar from '../../components/Navbar/Navbar';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import EditDonor from "../../components/EditDonor/EditDonor";
+import { Box } from "@mui/system";
+
+
+
+const DonorDetails = () => {
+
+const params= useParams();
+
+console.log("paramsId:", params.id, "params:", params)
+
+const [data, setData]=useState([])
+  const apiUrl = 'https://reviver-backend.herokuapp.com'
+  const token = localStorage.getItem('token')
+
+  const authAxios = axios.create({
+    baseURL:apiUrl,
+    headers: {Authorization :`Bearer ${token}`}
+  })
+      
+
+  useEffect(()=> {
+    authAxios.get(`${apiUrl}/donors/`+ params.id)
+    .then(res=> {
+      console.log("Data is", res.data)
+      setData(res.data)
+    })
+    .catch(err=>console.log(err))
+  }, 
+ // eslint-disable-next-line
+  [])
+
+  return (
+    <div className="list">
+      <Sidebar/>
+    
+      <div className='listContainer'>
+        <Navbar/>
+       
+        <Box id = "box-Donord">
+        <Grid container spacing={2} >
+      
+          <Grid item xs={6} md={6}>
+            <Paper elevation={2} sx={{ padding: 2 }} id="paper-data">
+              
+              <Typography variant="h5" id="data-text"> Donor Data</Typography>
+
+                  <div id="data-details">
+                    <div className="text-div">
+                      <div className="title-div">Name</div>
+                      <div className="details-div">{data.name}</div>
+                    </div>
+                    <div className="text-div">
+                      <div className="title-div">Surname</div>
+                      <div className="details-div">{data.surname}</div>
+                    </div>
+                    <div className="text-div">
+                      <div className="title-div">Email</div>
+                      <div className="details-div">{data.email}</div>
+                    </div>
+                    <div className="text-div">
+                      <div className="title-div">Phone Number</div>
+                      <div className="details-div">{data.phone}</div>
+                    </div>
+                    <div className="text-div">
+                      <div className="title-div">Choice of Job</div>
+                      <div className="details-div">{data.choiceOfJob}</div>
+                    </div>
+                    <div className="text-div">
+                      <div className="title-div">Location</div>
+                      <div className="details-div">{data.location}</div>
+                    </div>
+                    <div className="text-div">
+                      <div className="title-div">Duration</div>
+                      <div className="details-div">{data.duration}</div>
+                    </div>
+                    <div className="text-div">
+                      <div className="title-div">Date created</div>
+                      <div className="details-div">{(data.createdAt)}</div>
+                    </div>
+                  </div>
+
+              
+          </Paper>
+        </Grid>
+
+        <Grid item xs={6} md={6} >
+				
+            <EditDonor data={data} props={params.id}/>
+        </Grid>
+            
+   
+          
+      
+      </Grid>
+
+      </Box>
+
+    
+
+            
+        
+      </div>
+    </div>
+
+  
+
+
+  )
+}
+
+export default DonorDetails
